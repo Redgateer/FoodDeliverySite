c722a0eef63caabd2340762009a36b3257536a0a
Redgateer <redgateer17@gmail.com>
2024-08-22 18:45:55 -0400

    Adding initial template to repo

---
+++ reactTutorial/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose from a diverse menu featuring blah blah blah b;ah blah. only the finest ingredients grown locally to supply our customers with a fresh tasting meal.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ reactTutorial/frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu/>s
+    </div>
+  )
+}
+
+export default Home
